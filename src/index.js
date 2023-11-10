d435c0cf9b2d7ddf212fe4ce730ffcdcd4e0a578
Kim Gedie Arandid <101803250+b1ankK2@users.noreply.github.com>
2023-11-10 20:36:34 +0800

    changed web tab name

--- src/index.js
+++ src/index.js
@@ -1,86 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import ReactDOM from 'react-dom/client';
-// import Dashboard from './Pages/Dashboard';
-// import AddItem from './Pages/addItem';
-// import EditItem from './Pages/editItem';
-// import DeleteItem from './Pages/deleteItem';
-// import store from './redux/store';
-// import { Provider } from 'react-redux';
-// import sarisari from '../src/images/cart.gif'
-
-// // import react router
-// import {
-//     createBrowserRouter,
-//     RouterProvider,
-//   } from "react-router-dom";
-// import App from './App';
-
-// const router = createBrowserRouter([
-//     {
-//         path: '/',
-//         element: <IntroductionScreen/>,
-//     },
-//     {
-//         path: "Dashboard",
-//         element: <Dashboard />,
-//       },
-//       {
-//         path: "AddItem",
-//         element: <AddItem />,
-//       },
-//       {
-//         path: "EditItem",
-//         element: <EditItem />,
-//       },
-//       {
-//         path: "DeleteItem",
-//         element: <DeleteItem />,
-//       }    
-// ])
-
-// function IntroductionScreen() {
-//   const [redirect, setRedirect] = useState(false);
-
-
-
-
-//   useEffect(() => {
-//     const visitedIntro = sessionStorage.getItem('visitedIntro');
-//     if (visitedIntro) {
-//       setRedirect(true);
-//     } else {
-//       const timer = setTimeout(() => {
-//         setRedirect(true);
-//         sessionStorage.setItem('visitedIntro', true);
-//       }, 5000);
-//       return () => clearTimeout(timer);
-//     }
-//   }, []);
-
-//   if (redirect) {
-//     return <Dashboard />
-//   }
-
-//   return(
-//     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh', background: '#35475F' }}>
-//       <img src={sarisari} style={{ maxWidth: '100%', height: 'auto' }} />
-//       <h1 style={{color: 'white', letterSpacing: '5px'}}>Sari-Sari Store</h1>
-//     </div>
-//   )
-
-
-// }
-
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <Provider store={store}>
-//       <RouterProvider router={router}  />
-//     </Provider>
-
-
-// );
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from './Pages/Dashboard';
@@ -97,6 +14,8 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+
+document.title = "POS System";
 
 function IntroductionScreen() {
   const [redirect, setRedirect] = useState(false);
